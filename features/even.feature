bfe84d313abc8660cc5dfe96b12a3be7207daef5
jatejkemstersokadik <nem.de120@gmail.com>
2026-01-16 09:51:04 +0100

    .add

--- features/even.feature
+++ features/even.feature
@@ -1,8 +1,27 @@
 Feature: Páros szám ellenőrzése
 
-  # TODO: Írd ide a scenariókat!
-  # 1. Páros szám ellenőrzése (pl. 4)
-  # 2. Páratlan szám ellenőrzése (pl. 5)
-  # 3. Nulla ellenőrzése (0)
-  # 4. Negatív páros szám ellenőrzése (-4)
-  # 5. Negatív páratlan szám ellenőrzése (-5)
+  Scenario: Páros szám ellenőrzése
+    Given the number is 4
+    When I check the number
+    Then the result should be "even"
+
+  Scenario: Páratlan szám ellenőrzése
+    Given the number is 5
+    When I check the number
+    Then the result should be "odd"
+
+  Scenario: Nulla ellenőrzése
+    Given the number is 0
+    When I check the number
+    Then the result should be "even"
+
+  # Kiegészítő feladat: Negatív számok
+  Scenario: Negatív páros szám ellenőrzése
+    Given the number is -4
+    When I check the number
+    Then the result should be "even"
+
+  Scenario: Negatív páratlan szám ellenőrzése
+    Given the number is -5
+    When I check the number
+    Then the result should be "odd"
--- features/steps/even_steps.py
+++ features/steps/even_steps.py
@@ -1,21 +1,19 @@
 from behave import given, when, then
-# TODO: Importáld a number_checker modult a src mappából
+# Importáljuk a logikát a src mappából
+from src.number_checker import check_number
 
+@given('the number is {number}')
+def step_given_number(context, number):
+    # A kapott értéket egésszé (int) alakítjuk és elmentjük a context-be
+    context.number = int(number)
 
-# TODO: Implementáld a Given step-et
-# @given('the number is {number}')
-# def step_given_number(context, number):
-#     pass
+@when('I check the number')
+def step_when_check_number(context):
+    # Meghívjuk a függvényt az elmentett számmal, és az eredményt is elmentjük
+    context.result = check_number(context.number)
 
-
-# TODO: Implementáld a When step-et
-# Használd a check_number függvényt a src/number_checker.py fájlból!
-# @when('I check the number')
-# def step_when_check_number(context):
-#     pass
-
-
-# TODO: Implementáld a Then step-et
-# @then('the result should be "{expected}"')
-# def step_then_result(context, expected):
-#     pass
+@then('the result should be "{expected}"')
+def step_then_result(context, expected):
+    # Ellenőrizzük, hogy a kapott eredmény megegyezik-e az elvárttal
+    # Ha nem egyezik, hibaüzenetet dobunk
+    assert context.result == expected, f"Hiba! Kapott: {context.result}, Elvárt: {expected}"
